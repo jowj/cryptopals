3e1804a427175a54626d004489bb616434a9b730
jowj <me@jowj.net>
2018-09-09 13:14:30 -0500

    update to operate more obviously on bytes.

--- challenge1.py
+++ challenge1.py
@@ -1,6 +1,11 @@
 from binascii import unhexlify, b2a_base64
+import base64
 
 # Use the binascii.unhexlify() function to convert from a hex string to bytes. Then use the binascii.b2a_base64() function to convert that to Base64:
+hexstring = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+bytesThing = bytes.fromhex(hexstring)
 
-result = b2a_base64(unhexlify("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
-print(result)
+result = base64.b64encode(bytesThing)
+utfresult = result.decode("utf-8")
+
+print(utfresult)
