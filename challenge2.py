6cd445be8a56cd248e1ef8cd5132ea09fb3be8c0
jowj <me@jowj.net>
2018-09-09 13:53:33 -0500

    Remove unnecessary comment.

--- challenge2.py
+++ challenge2.py
@@ -1,10 +1,9 @@
-# WOW ok i made this initially like 80 times more complicated than I need to.
 
 def fixedXor(arg1, arg2):
     input1 = int(arg1, 16)
     input2 = int(arg2, 16)
 
     hexData = (hex(input1 ^ input2))
-    return hexData[2:]                # this is done to remove leading 0x. 
+    return hexData[2:]
 
 print(fixedXor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"))
